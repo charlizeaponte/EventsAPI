80fee3c0a7f690a39f810013ada621f4cf15e9e3
Charlize Aponte <charlizeaponte@yahoo.com>
2024-06-20 19:43:45 +0000

    added database

--- events.py
+++ events.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import pandas as pd 
+import sqlalchemy as db
 
 
 API_KEY = os.environ.get('EVENTS_CLIENT_ID')
@@ -14,7 +16,7 @@ endpoint = f"{BASE_URL}events.json"
 params = {
     "countryCode": "US",
     "apikey": os.environ.get('EVENTS_CLIENT_ID'),
-    "size": 1,  
+    "size": 5,  
     "keyword": user
 }
 
@@ -25,7 +27,30 @@ print(f"Response status code: {response.status_code}")
 # Checking if the request was successful
 if response.status_code == 200:
     # Print the JSON response
-    print(response.json())
+    events = response.json()['_embedded']['events']
+
+
+    events_list = []
+    for event in events:
+      events_info = {
+        "name":event["name"],
+        "id": event["id"],
+        "url": event["dates"]["start"]["localDate"],
+        "date": event["dates"]["start"].get("localTime","N/A"),
+        "time": event["_embedded"]["venues"][0]["name"],
+        "city": event["_embedded"] ["venues"][0]["city"]["name"]
+      }
+      events_list.append(events_info)
+    df = pd.DataFrame(events_list)
+
+    engine = db.create_engine('sqlite:///events.db')
+
+    df.to_sql('events',con=engine, if_exists='replace', index=False)
+
+    with engine.connect() as connection:
+      query_result = connection.execute(db.text("SELECT * FROM events;")).fetchall()
+      print(pd.DataFrame(query_result))
+
 else:
     print(f"Error: {response.status_code}")
     
