cc851fb6097fb9f00e4d1c4b666cff2d942a0906
Charlize Aponte <charlizeaponte@yahoo.com>
2024-06-20 15:40:59 +0000

    fixed api key

--- events.py
+++ events.py
@@ -1,9 +1,10 @@
 import requests
+import os
 
-API_KEY = "EnyhC0nnujH5iAVj6SKOteGi0jzV6N7V"
+API_KEY = os.environ.get('EVENTS_CLIENT_ID')
 BASE_URL = "https://app.ticketmaster.com/discovery/v2/"
 
-user = input("enter a performer:")
+user = input("Enter a performer: ")
 
 # Endpoint to get events
 endpoint = f"{BASE_URL}events.json"
@@ -11,15 +12,14 @@ endpoint = f"{BASE_URL}events.json"
 # Query parameters
 params = {
     "countryCode": "US",
-    "apikey": API_KEY,
+    "apikey": os.environ.get('EVENTS_CLIENT_ID'),
     "size": 1,  
     "keyword": user
 }
 
 # Making the GET request
 response = requests.get(endpoint, params=params)
-print(response.status_code)
-
+print(f"Response status code: {response.status_code}")
 
 # Checking if the request was successful
 if response.status_code == 200:
@@ -27,4 +27,4 @@ if response.status_code == 200:
     print(response.json())
 else:
     print(f"Error: {response.status_code}")
-    print(response.json())
+    
