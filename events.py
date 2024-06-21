fd4aee06856d232af0c4be3defec0f10863d8711
Charlize Aponte <charlizeaponte@yahoo.com>
2024-06-21 20:18:24 +0000

    Added code style

--- events.py
+++ events.py
@@ -1,8 +1,7 @@
-import requests
 import os
-import pandas as pd 
+import requests
+import pandas as pd
 import sqlalchemy as db
-
 
 API_KEY = os.environ.get('EVENTS_CLIENT_ID')
 BASE_URL = "https://app.ticketmaster.com/discovery/v2/"
@@ -16,7 +15,7 @@ endpoint = f"{BASE_URL}events.json"
 params = {
     "countryCode": "US",
     "apikey": os.environ.get('EVENTS_CLIENT_ID'),
-    "size": 5,  
+    "size": 5,
     "keyword": user
 }
 
@@ -29,28 +28,26 @@ if response.status_code == 200:
     # Print the JSON response
     events = response.json()['_embedded']['events']
 
-
     events_list = []
     for event in events:
-      events_info = {
-        "name":event["name"],
-        "id": event["id"],
-        "url": event["dates"]["start"]["localDate"],
-        "date": event["dates"]["start"].get("localTime","N/A"),
-        "time": event["_embedded"]["venues"][0]["name"],
-        "city": event["_embedded"] ["venues"][0]["city"]["name"]
-      }
-      events_list.append(events_info)
+        events_info = {
+            "name": event["name"],
+            "id": event["id"],
+            "url": event["dates"]["start"]["localDate"],
+            "date": event["dates"]["start"].get("localTime", "N/A"),
+            "venue": event["_embedded"]["venues"][0]["name"],
+            "city": event["_embedded"]["venues"][0]["city"]["name"]
+        }
+        events_list.append(events_info)
     df = pd.DataFrame(events_list)
 
     engine = db.create_engine('sqlite:///events.db')
 
-    df.to_sql('events',con=engine, if_exists='replace', index=False)
+    df.to_sql('events', con=engine, if_exists='replace', index=False)
 
     with engine.connect() as connection:
-      query_result = connection.execute(db.text("SELECT * FROM events;")).fetchall()
-      print(pd.DataFrame(query_result))
+        query_result = connection.execute(db.text("SELECT * FROM events;")).fetchall()
+        print(pd.DataFrame(query_result))
 
 else:
     print(f"Error: {response.status_code}")
-    
